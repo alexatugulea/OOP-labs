bf25090874c188961a85c97ede9cdf3795a67008
alexatugulea <43955272+alexatugulea@users.noreply.github.com>
2020-01-07 17:48:23 +0200

    Add files via upload

---
+++ Lab5/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+
+public class Main {
+    public static void main(String[] argv) {
+        J j = new J("A",
+                    "B",
+                    "C",
+                    "D",
+                    "E",
+                    "F",
+                    "G",
+                    "H",
+                    "I",
+                    "J",
+                    new X("The Name for X:"));
+
+        j.printProperty();
+    }
+}
---
+++ Lab5/X.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class X {
+    private String name;
+
+    public X(String name) {
+        this.name = name;
+    }
+
+    String getXName() {
+        return this.name;
+    }
+}
